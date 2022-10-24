4d9b5731fd2895474cade7e0a9d1c6979cc9a974
Dima <dimacheg06@gmail.com>
2022-10-24 22:47:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var obj = {'Коля':'100', 'Вася':'400', 'Петя':'800'};
+console.log('Зарплата Коли: ', obj['Коля'], 'грн');
+console.log('Зарплата Пети: ', obj['Петя'], 'грн');
+console.log('___________________')
+console.log('next exercise:')
+
+
+var arr = ['Привет', 'мир', '!'];
+arr[0] = 'Пока, ';
+alert(arr);
+
+var arr = [1, 2, 3, 4, 5];
+var arr = [];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+arr[3] = 4;
+arr[4] = 5;
+
+var arr = {
+    'ru':['голубой', 'красный', 'зеленый'],
+    'en':['blue', 'red', 'green'],
+};
+console.log('Должно вывести слово голубой: ', arr['ru'][0]);
+console.log('___________________')
+console.log('next exercise:')
+
+var arr = ['a', 'b', 'c', 'd'];
+console.log('Должно вывести a+b: ', arr[0], '+', arr[1]);
+console.log('Должно вывести c+d: ', arr[2], '+', arr[3])
+console.log('___________________')
+console.log('next exercise:')
+
+var obj = {'js':['jQuery', 'Angular'], 'php':'hello', 'css':'world'};
+console.log('Должно вывести jQuery: ', obj['js'][0]);
+console.log('___________________')
+console.log('next exercise:')
+
+var obj = {'1':'Monday', '2':'Tuesday', '3':'Wednesday', '4':'Thursday', '5':'Friday', '6':'Saturday', '7':'Sunday'};
+console.log('Today is ', obj['2'])
+console.log('___________________')
